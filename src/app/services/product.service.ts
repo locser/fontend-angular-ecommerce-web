470b22fc7a3f30de81e0f7c0a35f6d1aedc67c89
locser <ploc280301@gmail.com>
2022-11-28 19:04:13 +0700

    display Product List table

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient:HttpClient) {
+  }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<getResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+interface getResponse{
+  _embedded:{
+    products: Product[];
+  }
+}
